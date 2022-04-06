a00eaca750f656120354167db7d1bd3925b82b73
Sneha Parande <99521196+snehaparande@users.noreply.github.com>
2022-04-06 17:02:06 +0530

    Refactored
      Removed the function 'isPresent'.
      Renamed a function from 'locate' to 'locateGroup'.
      Refactored the function 'groupByIdentity'.

--- groupElementsTogether.js
+++ groupElementsTogether.js
@@ -1,47 +1,41 @@
+const isArray = Array.isArray;
+
 const areEqual = function (firstItem, secondItem) {
-  if (!(Array.isArray(firstItem) && Array.isArray(secondItem))) {
+  const areArrays = isArray(firstItem) && isArray(secondItem);
+  if (!areArrays) {
     return firstItem === secondItem;
   }
+
   if (firstItem.length !== secondItem.length) {
     return false;
   }
+
   for (let index = 0; index < firstItem.length; index++) {
     if (!areEqual(firstItem[index], secondItem[index])) {
-        return false;
-      }
+      return false;
     }
-    return true;
+  }
+  return true;
 };
 
-const locate = function (element, set) {
+const locateGroup = function (element, set) {
   for (let index = 0; index < set.length; index++) {
     if (areEqual(element, set[index][0])) {
       return index;
     }
   }
-};
-
-const isPresent = function (element, set) {
-  for (let index = 0; index < set.length; index++) {
-    if (areEqual(element, set[index][0])) {
-      return true;
-    }
-  }
-  return false;
+  return -1;
 };
 
 const groupByIdentity = function (set) {
-  if (set.length === 0) {
-    return [];
-  }
-  const groupedElements = [[set[0]]];
-  for (let index = 1; index < set.length; index++) {
-    if (isPresent(set[index], groupedElements)) {
-      const position = locate(set[index], groupedElements);
-      groupedElements[position].push(set[index]);
-    } else {
-      groupedElements.push([set[index]]);
+  const groupedElements = [];
+  for (let index = 0; index < set.length; index++) {
+    let position = locateGroup(set[index], groupedElements);
+    if (position === -1) {
+      position = groupedElements.length;
+      groupedElements.push([]);
     }
+    groupedElements[position].push(set[index]);
   }
   return groupedElements;
 };
